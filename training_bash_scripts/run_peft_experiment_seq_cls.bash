4a6cd882d169e87f8d35536f20e62221d8babfb5
titoghose <titoghose@gmail.com>
2023-07-13 16:12:45 +0100

    Added support for loading data info from yaml

--- training_bash_scripts/run_peft_experiment_seq_cls.bash
+++ training_bash_scripts/run_peft_experiment_seq_cls.bash
@@ -2,17 +2,19 @@
 # all tasks 50, 200, 500 samples - put tasks you wanna do in the tasks array separated by space
 # nlpie/tiny-biobert nlpie/bio-mobilebert 
                     
-model_name_or_path=(nlpie/tiny-biobert
-                    nlpie/distil-biobert 
-                    roberta-base
-                    emilyalsentzer/Bio_ClinicalBERT                                                                                                 
-                     )
-peft_methods=(Full) # LORA PREFIX PROMPT
+# model_name_or_path=(nlpie/tiny-biobert
+#                     nlpie/distil-biobert 
+#                     roberta-base
+#                     emilyalsentzer/Bio_ClinicalBERT                                                                                                 
+#                      )
+# peft_methods=(Full) # LORA PREFIX PROMPT
+model_name_or_path=(roberta-base)
+peft_methods=(LORA)
 task=icd9-triage-no-category-in-text
 max_epochs=5
 gpu=3
-data_dir=/mnt/sdd/efficient_ml_data/datasets/icd9-triage/no_category_in_text
-eval_data_dir=/mnt/sdd/efficient_ml_data/datasets/icd9-triage/no_category_in_text
+# data_dir=/mnt/sdd/efficient_ml_data/datasets/icd9-triage/no_category_in_text
+# eval_data_dir=/mnt/sdd/efficient_ml_data/datasets/icd9-triage/no_category_in_text
 log_save_dir=/mnt/sdd/efficient_ml_data/saved_models/peft/logs
 ckpt_save_dir=/mnt/sdd/efficient_ml_data/saved_models/peft/ckpts
 for model in "${model_name_or_path[@]}"
@@ -23,12 +25,9 @@ for model in "${model_name_or_path[@]}"
         python peft_trainer.py \
             --model_name_or_path "$model" \
             --max_epochs "$max_epochs" \
-            --training_data_dir "$data_dir" \
-            --eval_data_dir "$eval_data_dir" \
             --task "$task" \
             --peft_method "$peft_method" \
             --log_save_dir $log_save_dir \
             --ckpt_save_dir $ckpt_save_dir
-        
     done
 done
