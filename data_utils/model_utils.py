6a9d6a48e0331f689a1c60e66f07f0c8b854a76d
NtaylorOX <niall.taylor@psych.ox.ac.uk>
2023-09-29 14:59:01 +0100

    added functions to calculate model size/storage requirements

--- data_utils/model_utils.py
+++ data_utils/model_utils.py
@@ -1,5 +1,5 @@
 import torch
-
+import io
 
 
 # count trainable parameters
@@ -29,3 +29,28 @@ def unfreeze_model(model):
     # unfreeze all parameters
     for param in model.parameters():
         param.requires_grad = True
+        
+
+def get_model_size(model):
+    """Returns size of PyTorch model in bytes"""
+    model_size = sum(p.numel() for p in model.parameters() if p.requires_grad) * 4 / (1024**2)
+    print("Model size (MB):", model_size)
+    return model_size
+
+
+def get_full_model_size(model):
+    # Parameter sizes
+    param_size = sum(p.numel() * p.element_size() for p in model.parameters() if p.requires_grad)
+    
+    # Buffer sizes
+    buffer_size = sum(b.numel() * b.element_size() for b in model.buffers())
+    
+    # State dict sizes
+    state_dict = model.state_dict()
+    state_stream = io.BytesIO()
+    torch.save(state_dict, state_stream)
+    state_dict_size = len(state_stream.getbuffer())
+    
+    total_size = (param_size + buffer_size + state_dict_size) / (1024**2)
+    print("Total size (MB):", total_size)
+    return total_size
